c9cc7f735c62969243d776ca778f1e478e46a099
Spencer Dupre <spencer.dupre@gmail.com>
2015-09-23 23:37:27 -0400

    Added coinmaker.py

---
+++ coinmaker.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""
+This script produces new altcoins with specified parameters.
+"""
+__author__ = "Spencer Dupre"
+__copyright__ = "Copyright 2015"
+__credits__ = ["Spencer Dupre", "The Bitcoin Developers"]
+__license__ = "GPL"
+__version__ = "3.0"
+__maintainer__ = "Spencer Dupre"
+__email__ = "spencer.dupre@gmail.com"
+__status__ = "Alpha"
