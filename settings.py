01b6b5e4a67429923824a01d089213fb27939b30
Spencer Dupre <spencer.dupre@gmail.com>
2015-09-26 18:36:58 -0400

    Removed build options

--- settings.py
+++ settings.py
@@ -5,7 +5,3 @@ The settings for coinmaker.py
 class SETTINGS(object):
     source_dir = './sourcecoin'
     working_dir = './working'
-    autogen_command = "./autogen.sh"
-    config_command="""./configure"""
-    make_command="""make -s"""
-    os_ext = ""
